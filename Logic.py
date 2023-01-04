dd179afabd16af92fc62ec8b6cc8c3bf56262d61
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-05 00:08:51 +0500

    Added Logic module. it has codes for reversing, alphabetically sorting and randomising the list of words.

---
+++ Logic.py
@@ -0,0 +1,16 @@
+import sys
+import random
+
+
+class logic:
+    def ReversedList(self, Words):
+        Words.reverse()
+        return Words
+
+    def SortedList(self, Words):
+        Words.sort()
+        return Words
+
+    def Randomise(self, Words):
+        random.randint(Words)
+        return Words
