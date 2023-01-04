e5206fbf2b524c3956122f60b2205228a78d4f64
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-05 00:06:49 +0500

    Added code for user input as 5 words that is stored in a list. It generateds an error if user inputs les than 5 words

--- Program.py
+++ Program.py
@@ -1,1 +1,33 @@
+import sys
+from Logic import logic
 
+def main():
+    args = sys.argv[1:]
+        
+
+    if (len(args) != 5):
+        print ("error:Enter atleast 5 words")
+        print ("usage:Please enter 5 words")
+
+    else:
+        Words = []
+        Words = args
+
+        objL = logic()
+        objP = DisplayValues
+         
+    DisplayValues("Original List",Words)
+    DisplayValues("ReversedList",objL.ReversedList(Words))
+    DisplayValues("Sorted List",objL.SortedList(Words))
+    DisplayValues("Randomised List",objL.Randomise(Words))
+
+
+def  DisplayValues (heading, Words):
+    print(heading)
+    print("-----------------------------")
+    for word in Words:
+        print(word)
+        
+         
+if __name__ == '__main__':
+    main()
