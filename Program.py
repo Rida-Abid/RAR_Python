3224be51a5b742c19a571ac5f8f4621742d01aa8
Rida-Abid <107047216+Rida-Abid@users.noreply.github.com>
2023-01-07 01:39:01 +0500

    Changed the code for input that is not words.

--- Program.py
+++ Program.py
@@ -1,31 +1,43 @@
+from ast import Not
 import sys
+import string
 from Logic import logic
 
-def  DisplayValues (heading, Words):
-        print(heading)
-        print("-----------------------------")
-        for word in Words:
-            print(word)
+
 
 def main():
     args = sys.argv[1:]
         
+    try:
+        if (len(args) < 1):
+            print ("error:Enter atleast 2 words")
+            print ("usage:Please enter words")
+             
+        else:
+            Words = []
+            Words = args
+            objL = logic()
 
-    if (len(args) < 1):
-        print ("error:Enter atleast 2 words")
-        print ("usage:Please enter words")
-        return 
-
-    Words = []
-    Words = args
-    objL = logic()
+            def  DisplayValues (heading, Words):
+                print("-----------------------------")
+                print(heading)
+                print("-----------------------------")
+                for word in Words:
+                    print(word)
     
       
          
-    DisplayValues("Original List",Words)
-    DisplayValues("ReversedList",objL.ReversedList(Words))
-    DisplayValues("Sorted List",objL.SortedList(Words))
-    DisplayValues("Randomised List",objL.Randomise(Words))
+            DisplayValues("Original List",Words)
+            DisplayValues("ReversedList",objL.ReversedList(Words))
+            DisplayValues("Sorted List",objL.SortedList(Words))
+            DisplayValues("Randomised List",objL.Randomise(Words))
+    except ValueError:
+         for a in args:
+             for c in a:
+                if (c.isalpha()) == False:
+                    print("error:enter words ")
+         else:
+            print("")
 
 
     
